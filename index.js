f6001fc387f6ef80fea8aaba90b8f1ec1b6659af
Vadim <zhukov.vd.s@gmail.com>
2020-10-11 00:54:19 +0300

    2

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 const User = require('./models/user')
 const Transaction = require('./models/transaction')
 const keys = require('./keys/keys.dev.js')
-
-const PORT = process.env.PORT || 3000
+const homeRoutes = require('./routes/home')
 
 const app = express()
+
+// Creating handlebars engine
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs', 
+    helpers: require('./utils/hbs-helpers'),
+    allowProtoMethodsByDefault: true, 
+    allowProtoPropertiesByDefault: true
+})
+
+// Handlebars-engine register
+app.engine('hbs', hbs.engine)
+
+// Handlebars-engine activating 
+app.set('view engine', 'hbs')
+
+// Pages folder register (folder views)
+app.set('views', 'views')
+
+// Styles and images folder register
+//app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
+
+
+
+
+// Routes 
+app.use('/home', homeRoutes)
+
+
+
+
+const PORT = process.env.PORT || 3000
 
 async function start() {
     try {
@@ -23,11 +56,13 @@ async function start() {
         console.log(e)
     }
 
+    /*
     const user = new User({user_id: '1', login: 'pankus22', password: '1234', name: 'Vadim'})
     await user.save()
 
     const transaction = new Transaction({user_id: '1', transaction_id: '1', amount: 1234, region: 'Russia'})
     await transaction.save()
+    */
 }
 
 start()
